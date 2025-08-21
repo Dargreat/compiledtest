9020051c2d6160bb1d32661d709f9b0c99d01e03
Dargreat <132403953+Dargreat@users.noreply.github.com>
2025-08-22 00:58:29 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverActions: true, // enables server actions
-  },
-  // add more config options here if needed
+  // Add other config options if needed
 };
 
 module.exports = nextConfig;
